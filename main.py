8d26d7e6ae88c05006f33882ac8722bb717d62c5
Phạm Đức Hiếu <21520856@gm.uit.edu.vn>
2024-11-17 04:53:53 +0700

    Phạm Đức Hiếu
    Create the food

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-import pygame
+import pygame   
 import time
 import random
-
+from pygame.math import Vector2
 
 pygame.init()
 
 GREEN = (173, 204, 96)
 DARK_GREEN = (43, 51, 24)
-screen = pygame.display.set_mode ((750,750))
+
+cell_size = 30
+number_of_cells = 25
+
+class Food:
+    def __init__(seft):
+        seft.position = Vector2(5,6)
+    
+    def draw(seft):
+        food_rect = pygame.Rect(seft.position.x * cell_size, seft.position.y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, DARK_GREEN, food_rect)
+
+screen = pygame.display.set_mode ((cell_size*number_of_cells, cell_size*number_of_cells))
 
 pygame.display.set_caption("Snake Game")
 
 clock = pygame.time.Clock()
+
+food = Food()
 
 while True:
     for event in pygame.event.get():
@@ -20,5 +34,6 @@ while True:
             sys.exit()
             
     screen.fill(GREEN)        
+    food.draw()
     pygame.display.update()
     clock.tick(60)
